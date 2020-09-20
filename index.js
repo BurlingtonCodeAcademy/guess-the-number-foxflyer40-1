f6ffaec71c9af8b212a04c45ad9c4f03077ae20e
foxflyer40 <60403841+foxflyer40@users.noreply.github.com>
2020-09-20 19:19:40 -0400

    fix NaN check in cheatChecker

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ let hiLo = ""
 let numOfGuesses = 0
 
 // GLOBALFUNCTIONS
+
+
 function showVariables() {
   console.log('\n')
   console.log("hiNum = " + hiNum + " " + typeof hiNum)
@@ -41,9 +43,13 @@ function newGuess(max, min) {
 }
 
 function cheatChecker(cheatCode) {
+
   if (cheatCode === 1) {
-    if (secretNumber < loNum || secretNumber > hiNum || secretNumber === NaN) {
+    if (secretNumber < 1 || secretNumber > hiNum) {
       console.log("\nYou are trying to cheat.\nNext time pick a number in range.")
+      process.exit()
+    } else if (isNaN(secretNumber)) {
+      console.log("\nLetters are not allowed.\nNext time pick a NUMBER in range.")
       process.exit()
     }
   } else if (cheatCode === 2) {
@@ -61,26 +67,23 @@ function cheatChecker(cheatCode) {
 
 function randomInteger(min, max) {
   let range = max - min + 1;
-  return min + Math.floor(Math.random() * range);
+  return +(min + Math.floor(Math.random() * range));
 }
 
 start()
 
 async function start() {
-  console.log("\nLet's play a game where one of us picks a number inside of a range\nand the other tries to guess it\n")
-  gameVersion = await ask("Do you want to be the number picker or number guesser?\nEnter P or G \n>")
+  console.log("\nLet's play a game where one of us picks a number inside of a range\nand the other tries to guess it.\n")
+  gameVersion = await ask("Do you want to be the number picker or number guesser?\nEnter P for picker\nEnter G for guesser\n>")
   if (gameVersion === 'P') {
     computerGuesses()
   } else if (gameVersion === 'G') {
     humanGuesses()
   } else {
-    console.log("Pick capital P or capital G only")
+    console.log("Pick capital P or capital G only.\nPlease start over.")
     process.exit()
   }
 }
-
-
-
 
 // Run Human Guesses game
 async function humanGuesses() {
@@ -104,9 +107,8 @@ async function humanGuesses() {
   if (numOfGuesses <= 7) {
     console.log("You must have cheated to guess it in only " + numOfGuesses + " tries.")
   } else {
-    console.log("It took you " + numOfGuesses + " tries.")
+    console.log("HA! It took you " + numOfGuesses + " tries.")
   }
-  showVariables() //progress check
   process.exit()
 }
 
@@ -116,20 +118,17 @@ async function computerGuesses() {
   numOfGuesses = 1
   console.log("\nNow You (Puny human) will pick a number and\nI (Mighty computer) will certainly guess it.\n")
   // Allow Human to set range
-  hiNum = Number(await ask("\nI will even allow you to set the range.\nLet's say bettween 1 and...?\nEnter a number greater than 1 \n>"))
+  hiNum = +(await ask("\nI will even allow you to set the range.\nLet's say bettween 1 and...?\nEnter a number greater than 1 \n>"))
   // Human picks secret number
-  secretNumber = Number(await ask("\nNow, pick your secret number.\n>(I will not look... really...)\n>"))
+  secretNumber = +(await ask("\nNow, pick your secret number.\n>(I will not look... really...)\n>"))
+  showVariables()
   cheatChecker(1)  // check Human input for cheating
   console.log('You entered: ' + secretNumber + "\n(Which I TOTALLY did not see.)\n")
 
   // start loop
   while (guessConfirm !== 'Y') {
     currentGuess = newGuess(hiNum, loNum)
-
-    showVariables()
-
     guessConfirm = await ask("is your number " + currentGuess + "?\nEnter Y or N\n>")
-
     if (guessConfirm === 'Y') {
       console.log('\nWOOHOO!\nI guessed it in ' + numOfGuesses + ' tries!')
     } else if (guessConfirm === 'N') {
@@ -145,10 +144,9 @@ async function computerGuesses() {
         hiNum = currentGuess
       } else { 'Please enter Capital H or Capital L only...\n' }
     } else {
-      console.log('Please enter Capital Y or Capital N only...\n')
+      console.log('Please enter Capital Y or Capital N only...OK?\n')
     }
   }
 
-  showVariables() //progress check
   process.exit()
 }
