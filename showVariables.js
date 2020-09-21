4dd6f2ccbb591bd1c8d37d4266a97449ccad00c3
foxflyer40 <60403841+foxflyer40@users.noreply.github.com>
2020-09-21 08:41:22 -0400

    fix number of tries bug

---
+++ showVariables.js
@@ -0,0 +1,13 @@
+function showVariables() {
+     console.log('\n')
+     console.log("hiNum = " + hiNum + " " + typeof hiNum)
+     console.log("loNum = " + loNum + " " + typeof loNum)
+     console.log("secretNumber = " + secretNumber + " " + typeof secretNumber)
+     console.log("currentGuess = " + currentGuess + " " + typeof currentGuess)
+     console.log('guessHigherThan = ' + guessHigherThan + " " + typeof guessHigherThan)
+     console.log('guessLowerThan = ' + guessLowerThan + " " + typeof guessLowerThan)
+     console.log("guessConfirm = " + guessConfirm + " " + typeof guessConfirm)
+     console.log("hiLo = " + hiLo + " " + typeof hiLo)
+     console.log("NumOfGuesses " + numOfGuesses + " " + typeof numOfGuesses)
+     console.log('\n')
+}
